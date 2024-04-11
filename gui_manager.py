03066cd8a5744a7fe04dc7ed68924e641f6333ac
sshb <sshb@live.com>
2024-04-11 15:54:41 +0800

    sperated GUI file

---
+++ gui_manager.py
@@ -0,0 +1,119 @@
+import dearpygui.dearpygui as dpg
+from state import app_state
+from schedule import ConstantIntervalSchedule, CsvSchedule
+import asyncio
+
+class GUIManager:
+    def __init__(self, equipment_list, data_manager):
+        self.equipment_list = equipment_list
+        self.data_manager = data_manager
+
+    def setup(self):
+        dpg.create_context()
+        with dpg.theme() as disabled_theme:
+            with dpg.theme_component(dpg.mvButton, enabled_state=False):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (100, 100, 100), category=dpg.mvThemeCat_Core)
+        dpg.bind_theme(disabled_theme)
+        with dpg.window(label="Data Visualization", tag="main_window"):
+            with dpg.group(horizontal=True):
+                dpg.add_text("Equipment connected and press start ----->")
+                dpg.add_button(label="Start", width=75, callback=self.start_stop_handler, tag="start_stop_button")
+                dpg.add_button(label="Save", width=75, callback=self.save_handler, tag="save_button")
+            with dpg.group(horizontal=False):
+                self.setup_live_plot()
+
+            with dpg.group(horizontal=False):
+                self.setup_progress_plot(self.equipment_list)
+
+        dpg.set_exit_callback(self.on_attempt_to_close)
+        dpg.create_viewport(title='Monitoring Dashboard', width=1024, height=850, disable_close=True)
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        dpg.set_primary_window("main_window", True)
+
+    ### GUI---run live plot
+    def setup_live_plot(self):
+        with dpg.plot(label="Sensor Data", width=1024, height=600, tag="sensor_plot"):
+            dpg.add_plot_legend()
+            # Note: Removed direct series creation from here, it will be handled dynamically
+            dpg.add_plot_axis(dpg.mvXAxis, label="Time", time=True, tag="x_axis")
+            dpg.add_plot_axis(dpg.mvYAxis, label="Data", tag="y_axis")
+    
+    def saving_data(self):
+        asyncio.run(self.data_manager.save_data())
+    ### GUI---Start button
+    def save_handler(self, sender, app_data, user_data):
+        print("saving....")
+        self.saving_data()
+
+    ### GUI---Exit button
+    def exit_handler(self):
+        print("Saving data before exit...")
+        # Add your data saving logic here.
+        dpg.stop_dearpygui()
+    def show_exit_confirmation_modal(self):
+        if not dpg.does_item_exist("exit_confirmation_modal"):
+            with dpg.window(label="Confirm Exit", modal=True, no_close=True, tag="exit_confirmation_modal"):
+                dpg.add_text("Are you sure you want to exit?")
+                with dpg.group(horizontal=True, ):
+                    dpg.add_button(label="Yes", width=75, callback=self.exit_handler)
+                    dpg.add_button(label="No", width=75, callback=lambda: dpg.delete_item("exit_confirmation_modal"), before="Yes")
+
+    def on_attempt_to_close(self):
+        self.show_exit_confirmation_modal()
+        return True  # Prevent the default close operation
+
+    ### GUI---run progress plot---only for step plot
+    def transform_for_step_plot(self,schedule_data):
+        """
+        Transform schedule data for a hold-last-value (step) curve.
+        Each point is duplicated with the next point's timestamp to create a step effect.
+        """
+        step_data = []
+        for i, (time, value) in enumerate(schedule_data):
+            if i < len(schedule_data) - 1:
+                # Duplicate the current point with the next point's timestamp
+                next_time = schedule_data[i + 1][0]
+                step_data.append((time, value))
+                step_data.append((next_time, value))
+            else:
+                # Last point, just append
+                step_data.append((time, value))
+        return step_data
+
+    ### GUI---run progress plot
+    def setup_progress_plot(self,equipment_list):
+        try:
+            with dpg.plot(label="Progress", width=1024, height=200, tag="pro_plot"):
+                dpg.add_plot_legend()
+                x_axis = dpg.add_plot_axis(dpg.mvXAxis, label="Overall Time (s)")
+                y_axis_p = dpg.add_plot_axis(dpg.mvYAxis, label="Value")
+                # Assuming x_axis and y_axis are correctly set up
+                dpg.add_vline_series([0], parent=x_axis, label="Current Progress", tag="progress_marker")
+                for equipment in equipment_list:
+                    if isinstance(equipment.schedule, CsvSchedule):
+                        # times, values = zip(*equipment.schedule.schedule_data)
+                        # dpg.add_line_series(times, values, parent=y_axis_p, label=equipment.name)
+                        # Transform the original schedule data for a step plot
+                        step_data = self.transform_for_step_plot(equipment.schedule.schedule_data)
+                        times, values = zip(*step_data)
+                        dpg.add_line_series(times, values, parent=y_axis_p, label=equipment.name)
+        except Exception as e:
+            print(f"Error while creating progress plot: {e}")
+
+    ### GUI---Start button
+    def start_stop_handler(self,sender, app_data, user_data):
+        current_label = dpg.get_item_label(sender)
+
+        if current_label == "Start":
+            dpg.set_item_label(sender, "Stop")
+            dpg.disable_item("save_button")
+            # Signal tasks to stop
+            app_state.start()
+        else:
+            dpg.set_item_label(sender, "Start")
+            dpg.enable_item("save_button")
+            # Signal tasks to stop
+            app_state.stop()
+
+
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import yaml
 import importlib
-from schedule import ConstantIntervalSchedule, CsvSchedule
+import time
 import asyncio
 from asyncio import Event
-from datamanager import AsyncDataManager
-import dearpygui.dearpygui as dpg
-import time
-from state import app_state
 
+import dearpygui.dearpygui as dpg
+
+from state import app_state
+from gui_manager import GUIManager
+from datamanager import AsyncDataManager
+from schedule import ConstantIntervalSchedule, CsvSchedule
 
 ## config file
 def load_config(filename):
@@ -51,117 +53,37 @@ async def update_progress_marker():
             print("Progress marker does not exist yet.")
         await asyncio.sleep(1)
 
-### GUI---run live plot
-def setup_live_plot():
-    with dpg.plot(label="Sensor Data", width=1024, height=600, tag="sensor_plot"):
-        dpg.add_plot_legend()
-        # Note: Removed direct series creation from here, it will be handled dynamically
-        dpg.add_plot_axis(dpg.mvXAxis, label="Time", time=True, tag="x_axis")
-        dpg.add_plot_axis(dpg.mvYAxis, label="Data", tag="y_axis")
 
-### GUI---run progress plot---only for step plot
-def transform_for_step_plot(schedule_data):
-    """
-    Transform schedule data for a hold-last-value (step) curve.
-    Each point is duplicated with the next point's timestamp to create a step effect.
-    """
-    step_data = []
-    for i, (time, value) in enumerate(schedule_data):
-        if i < len(schedule_data) - 1:
-            # Duplicate the current point with the next point's timestamp
-            next_time = schedule_data[i + 1][0]
-            step_data.append((time, value))
-            step_data.append((next_time, value))
-        else:
-            # Last point, just append
-            step_data.append((time, value))
-    return step_data
-### GUI---run progress plot
-def setup_progress_plot(equipment_list):
-    try:
-        with dpg.plot(label="Progress", width=1024, height=200, tag="pro_plot"):
-            dpg.add_plot_legend()
-            x_axis = dpg.add_plot_axis(dpg.mvXAxis, label="Overall Time (s)")
-            y_axis_p = dpg.add_plot_axis(dpg.mvYAxis, label="Value")
-            # Assuming x_axis and y_axis are correctly set up
-            dpg.add_vline_series([0], parent=x_axis, label="Current Progress", tag="progress_marker")
-            for equipment in equipment_list:
-                if isinstance(equipment.schedule, CsvSchedule):
-                    # times, values = zip(*equipment.schedule.schedule_data)
-                    # dpg.add_line_series(times, values, parent=y_axis_p, label=equipment.name)
-                    # Transform the original schedule data for a step plot
-                    step_data = transform_for_step_plot(equipment.schedule.schedule_data)
-                    times, values = zip(*step_data)
-                    dpg.add_line_series(times, values, parent=y_axis_p, label=equipment.name)
-    except Exception as e:
-        print(f"Error while creating progress plot: {e}")
 
-### GUI---Start button
-def start_stop_handler(sender, app_data, user_data):
-    current_label = dpg.get_item_label(sender)
 
-    if current_label == "Start":
-        dpg.set_item_label(sender, "Stop")
-        dpg.disable_item("save_button")
-        # Signal tasks to stop
-        app_state.start()
-    else:
-        dpg.set_item_label(sender, "Start")
-        dpg.enable_item("save_button")
-        # Signal tasks to stop
-        app_state.stop()
 
-def saving_data():
-    asyncio.run(data_manager.save_data())
 
-### GUI---Start button
-def save_handler(sender, app_data, user_data):
-    print("saving....")
-    saving_data()
-### GUI---Exit button
-def exit_handler():
-    print("Saving data before exit...")
-    # Add your data saving logic here.
-    dpg.stop_dearpygui()
+# ## main GUI
+# async def setup_dpg(equipment_list):
+#     dpg.create_context()
 
-def show_exit_confirmation_modal():
-    if not dpg.does_item_exist("exit_confirmation_modal"):
-        with dpg.window(label="Confirm Exit", modal=True, no_close=True, tag="exit_confirmation_modal"):
-            dpg.add_text("Are you sure you want to exit?")
-            with dpg.group(horizontal=True, ):
-                dpg.add_button(label="Yes", width=75, callback=exit_handler)
-                dpg.add_button(label="No", width=75, callback=lambda: dpg.delete_item("exit_confirmation_modal"), before="Yes")
+#     with dpg.theme() as disabled_theme:
+#         with dpg.theme_component(dpg.mvButton, enabled_state=False):
+#             dpg.add_theme_color(dpg.mvThemeCol_Text, (100, 100, 100), category=dpg.mvThemeCat_Core)
+#     dpg.bind_theme(disabled_theme)
 
-def on_attempt_to_close():
-    show_exit_confirmation_modal()
-    return True  # Prevent the default close operation
+#     with dpg.window(label="Data Visualization", tag="main_window"):
+#         # Create a group for the live plot
+#         with dpg.group(horizontal=True, ):
+#             dpg.add_text("Equipment connected and press start ----->")
+#             dpg.add_button(label="Start", width=75, callback=start_stop_handler, tag="start_stop_button")
+#             dpg.add_button(label="Save", width=75,  callback=save_handler, tag="save_button")
+#         with dpg.group(horizontal=False):
+#             setup_live_plot()
 
-## main GUI
-async def setup_dpg(equipment_list):
-    dpg.create_context()
-
-    with dpg.theme() as disabled_theme:
-        with dpg.theme_component(dpg.mvButton, enabled_state=False):
-            dpg.add_theme_color(dpg.mvThemeCol_Text, (100, 100, 100), category=dpg.mvThemeCat_Core)
-    dpg.bind_theme(disabled_theme)
-
-    with dpg.window(label="Data Visualization", tag="main_window"):
-        # Create a group for the live plot
-        with dpg.group(horizontal=True, ):
-            dpg.add_text("Equipment connected and press start ----->")
-            dpg.add_button(label="Start", width=75, callback=start_stop_handler, tag="start_stop_button")
-            dpg.add_button(label="Save", width=75,  callback=save_handler, tag="save_button")
-        with dpg.group(horizontal=False):
-            setup_live_plot()
-
-        with dpg.group(horizontal=False):
-            setup_progress_plot(equipment_list)
-    # dpg.show_metrics()
-    dpg.set_exit_callback(on_attempt_to_close)
-    dpg.create_viewport(title='Monitoring Dashboard', width=1024, height=850, disable_close=True)
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
-    dpg.set_primary_window("main_window", True)
+#         with dpg.group(horizontal=False):
+#             setup_progress_plot(equipment_list)
+#     # dpg.show_metrics()
+#     dpg.set_exit_callback(on_attempt_to_close)
+#     dpg.create_viewport(title='Monitoring Dashboard', width=1024, height=850, disable_close=True)
+#     dpg.setup_dearpygui()
+#     dpg.show_viewport()
+#     dpg.set_primary_window("main_window", True)
 
 async def task_monitor(data_manager, equipment_list):
     tasks = []
@@ -192,15 +114,23 @@ async def task_monitor(data_manager, equipment_list):
 
 ## main logic
 async def main():
+    gui_manager = GUIManager(equipment_list, data_manager)
+    gui_manager.setup()
+
+
 
     # Start monitoring tasks based on app_state
     asyncio.create_task(task_monitor(data_manager, equipment_list))
 
-    await setup_dpg(equipment_list)
     # Start rendering in a non-blocking way
     while dpg.is_dearpygui_running():
         dpg.render_dearpygui_frame()
         await asyncio.sleep(0.016)  # Roughly 60 FPS
+    # await setup_dpg(equipment_list)
+    # # Start rendering in a non-blocking way
+    # while dpg.is_dearpygui_running():
+    #     dpg.render_dearpygui_frame()
+    #     await asyncio.sleep(0.016)  # Roughly 60 FPS
 
     dpg.destroy_context()
 
